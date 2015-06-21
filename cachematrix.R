f7060bb9d297709065e1c67be04d5feec7f51db4
achandar <aishwarya.chandar@gmail.com>
2015-06-22 00:07:18 +0530

    Updating cachematrix.R code
    Update the functions to reflect the assignment requirements

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+        # x is a square invertible matrix
+        # return: a list containing functions to
+        #             1. set the matrix
+        #              2. get the matrix
+        #              3. set the inverse of the matrix
+        #              4. get the inverse of the matrix
+        # These lists are used as input for cacheSolve()
+        inv = NULL
+        set = function(y) {
+                x <<- y #from Example : used to assign a value to an object in an environment 
+                #that is different from the current environment
+                inv <<- NULL
+        }
+        get = function() x
+        setinverse = function(inverse) inv <<- inverse 
+        getinverse = function() inv
+        list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        # x: output of makeCacheMatrix()
+        # return: inverse of the original matrix input to makeCacheMatrix()
+        
+        inv = x$getinverse()
+        
+        # if the inverse has already been calculated
+        if (!is.null(inv)){
+                # get it from the cache and skips computational overhead 
+                message("getting cached data")
+                return(inv)
+        }
+        
+        # If it's not in cache, get the inverse 
+        mat.data = x$get()
+        inv = solve(mat.data, ...)
+        
+        # sets the value of the inverse in the cache
+        x$setinverse(inv)
+        
+        return(inv)
 }
